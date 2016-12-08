bd05c835e6becc6e3cb46cf1ded8a48495df5a05
amci1203 <allen.mcintoshii@gmail.com>
2016-12-08 12:37:35 -0500

    Added css modules from my website

--- src/components/main-nav.jsx
+++ src/components/main-nav.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import Links from './nav-links.jsx';
+import Links from './reusables/nav-links.jsx';
 
 export default function MainNav (props) {
   return (
     <nav className='main-nav'>
       <div className='wrapper'>
-        <h1 className='main-nav__heading'>{ props.candidateName }</h1>
-        <button className='main-nav__toggle' />
+        <h1 className='main-nav__heading'><a href='http://allen-mcintoshii.com'>{ props.candidateName }</a></h1>
+        <button className='main-nav__toggle' ><span className='main-nav__toggle__middle' /></button>
         <div className='main-nav__nav-links'>
           <Links links={['about', 'skills', 'contacts']}/>
         </div>
